90cf4108e2c23b4c0cf5614f92c72f3dd61afa10
doctype-Melvin <doctype.melvin@gmail.com>
2022-12-14 08:49:27 +0100

    adds personal details and footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-
+import github from '../assets/github-logo.png'
 
 function Footer(){
     return (
         <div className="footer">
-
+            <a href="https://www.github.com/doctype-Melvin">
+                <img className="git" src={github}>
+                </img>
+            </a>
         </div>
     )
 }
+
+export default Footer
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,17 @@ function Main(){
     return (
         <div>
             <span className="sectionHeader">About</span>
-            <p className="sectionPara"></p>
+            <p className="sectionPara">
+                I am a self-taught Web Developer and I am looking 
+                for an opportunity to take responsibility in my first professional
+                Junior role. My learning resources have been <a className="resource" href="https://www.freecodecamp.org/">freeCodeCamp.org</a> and
+                <a className="resource" href="https://www.theodinproject.com/about">TheOdinProject</a>.
+
+            </p>
             <span className="sectionHeader">Interests</span>
-            <p className="sectionPara"></p>
+            <p className="sectionPara">
+                Weightlifting, Sports, Music, Music Production, Painting, Web Culture
+            </p>
         </div>
     )
 }
